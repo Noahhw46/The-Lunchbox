d026871ca7843560ae44f36372eb5f24131bccd8
Noahhw46 <Noahhw46@gmail.com>
2022-11-09 10:50:32 -0500

    deleted:    test2.py

--- newdir/someclasses.py
+++
@@ -1,7 +0,0 @@
-class Spider:
-    
-    def __init__(self, name):
-        self.name = name
-
-    def say_hi(self):
-        print(f"Hi, my name is {self.name}")
--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-from hello import Dog
-from newdir.someclasses import Spider 
-
-newdog = Dog("Guy", "brown", ["bark", "eat"])
-newdog.eat()
-
-newspider = Spider("Bob")
-newspider.say_hi()
