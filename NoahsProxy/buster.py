5e8e7c6c210e612fa9479611b3dd57e8aaf7b645
Noahhw46 <Noahhw46@gmail.com>
2022-11-09 19:18:21 -0500

    Co-authored-by: notmosaad <notmosaad@users.noreply.github.com> Co-authored-by: Jordan Stroud <AmericanSyrup@users.noreply.github.com> Co-authored-by: ExitTheScholar <ExitTheScholar@users.noreply.github.com>

--- NoahsProxy/buster.py
+++ NoahsProxy/buster.py
@@ -6,8 +6,15 @@ import sys
 
 #usage: python3 buster.py <url> <wordlist>
 
-url = sys.argv[1]
-wordlist = sys.argv[2]
+#protocol = input(f"Do you want HTTP or HTTPS?\n").lower() ###POSSIBLY UNNEEDED. IT'S HERE INCASE
+name_file = input("What would you like to name your output file? Leave empty for none:\n")
+url = input(f"What URL are we busting today?\n").lower()
+if 'www.' not in url:
+    url = f"www.{url}"
+    if 'http' not in url:
+        url = f"https://{url}"
+wordlist = input(f"What is the word list you want to use? (Full or Relative path to word list):\n")
+
 
 with open(wordlist, 'r') as f:
     all_lines = f.readlines()
@@ -15,11 +22,26 @@ with open(wordlist, 'r') as f:
 for line in all_lines:
         line = line.strip()
         response = requests.get(f"{url}/{line}")
+        tosave_successes = []
+        tosave_failures = []
         try:
             response.raise_for_status()
             response = response.text
             print(f'{url}/{line}')
+            tosave_successes.append(f'{url}/{line}')
         except:
             print(f"{response} from {url}/{line}")
+            tosave_failures.append(f"{response} from {url}/{line}")
+
+if name_file != "":
+    with open(f'{name_file}_successes.txt', 'w') as f:
+        for item in tosave_successes:
+            f.write(f"{item}\n")
+
+    with open(f'{name_file}_failures.txt', 'w') as f:
+        for item in tosave_failures:
+            f.write(f"{item}\n")
+        
+cont = input("Would you like to mine this bust?")
 
 
---
+++ param_miner.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import requests
+import random
+import time
+import regex as re
+
+
+def extract_parms(url, placeholder):
+    #regex to extract parameters
+    params = rf"(\?|&)([^=]+)={placeholder}"
+    result = re.findall(params, url)
+    return result
+
+def makerequest(url):
+
+    with open('useragents.txt', 'r') as f:
+        agents = f.readlines()
+        agents = [agent.strip() for agent in agents]
+    agent = random.choice(agents)
+
+    headers = {'User-Agent': agent}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        result = response.text
+        
+    except requests.exceptions.ConnectionError as e:
+            print("Can not connect to server.")
+    except requests.exceptions.Timeout as e:
+            print("Timeout Error.")
+            time.sleep(2)
+    except requests.exceptions.HTTPError as err:
+            print(f"{err}. Retrying in 2 seconds.")
+            time.sleep(2)
+    except requests.exceptions.RequestException as e:
+            print(" {e} Can not get target information")
+    except KeyboardInterrupt as k:
+            print("Interrupted by user. Exiting.")
+            raise SystemExit(k)    
+    finally:
+        return result
+
+
+
+
+def main():
+    domain = input("What domain would you like to mine from? \n")
+    outfile = input("What output file would you like to use? \n")
+    placeholder = input("What string as a placeholder after the parameter would you like to use? \n")
+
+    url =  f"https://web.archive.org/cdx/search/cdx?url=*.{domain}/*&output=txt&fl=original&collapse=urlkey&page=/"
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
