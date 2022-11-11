566fd4ac491e8155dc5fffac82944bd068422606
Noahhw46 <98559180+Noahhw46@users.noreply.github.com>
2022-11-11 13:27:13 -0500

    Delete test_dict.py

--- mitm_attack/test_dict.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-dict_of_friends_and_grades = {'John': [90, 60, 50], 'Jane': [80, 30, 10], 'Jack': [20, 10, 5]}
-
-for k in dict_of_friends_and_grades.keys():
-    print(dict_of_friends_and_grades[k][1])
-
-#This works!
