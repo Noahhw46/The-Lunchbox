b07be2922497ca1c8d548d1cae24206475c429eb
Noahhw46 <Noahhw46@gmail.com>
2022-11-11 16:52:15 -0500

    modified:   buster/buster_recursive.py

--- buster/buster_recursive.py
+++ buster/buster_recursive.py
@@ -1,5 +1,9 @@
 #!/bin/usr/env python3
 
+#I think it would be cool to have a "recursive" checkbox that would run this logic instead of the normal one, but I'm not sure how to do that with the gui so lmk
+
+
+
 import requests
 
 def bust(url, wordlist, succesful_urls):
